56b30d8758c06f79dd84a16069b89ca4aeab5e3c
Timothee Deve <timdeve@users.noreply.github.com>
2016-02-15 16:34:48 +0000

    Adds nickname

--- public/app/controllers/messagesController.js
+++ public/app/controllers/messagesController.js
@@ -9,9 +9,14 @@ function messagesController($scope){
 
   var socket = io()
 
+  var date = String(Date.now())
+
+  var shortDate = date.substr(date.length - 4, date.length)
+
   self.all = []
   self.message = ''
   self.sendMessage = sendMessage
+  self.user = 'Guest' + shortDate
 
   socket.on('chat message', function(msg){
     self.all.push(msg)
@@ -20,8 +25,17 @@ function messagesController($scope){
 
 
   function sendMessage() {
-     socket.emit('chat message', self.message)
-     self.message = ''
+    var substr = self.message.substr(0, 6)
+
+    if ( substr === "/nick ") {
+      self.user = self.message.substr(6, self.message.length)
+      self.message = ''
+    }
+    else {
+      socket.emit('chat message', {user: self.user, message: self.message})
+      self.message = ''
+    }
+
   }
 
 
