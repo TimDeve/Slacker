da906526b23b3affd7ca06bcff59e191ccb7b007
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-02-15 12:15:39 +0100

    AngularStart

--- public/app/app.js
+++ public/app/app.js
@@ -1,16 +1,2 @@
-var socket = io();
-
-$(document).ready(function() {
-
-  $('form').submit(function(event){
-    event.preventDefault()
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-  });
-
-  socket.on('chat message', function(msg){
-    $('#messages').append(msg);
-  });
-
-})
+angular
+   .module('giphyMessenger', ['ngWebSocket'])
